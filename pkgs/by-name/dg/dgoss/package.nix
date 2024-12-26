3c11615ce842cffb5ec5264515ac91930590aa7a
Miroslav Vadkerti <mvadkert@redhat.com>
2024-12-26 02:53:17 +0100

    dgoss: reference goss binary with GOSS_PATH
    It is needed to volume mount the real binary into the container, not the
    bash wrapper goss nix package provides.
    
    Without this patch:
    
    ```
    ❯ dgoss edit e321bdcd5137
    INFO: Starting podman container
    INFO: Container ID: 27895516
    INFO: Run goss add/autoadd to add resources
    
    sh-5.2# goss
    sh: /goss/goss: cannot execute: required file not found
    
    sh-5.2# cat /goss/goss
    PATH=${PATH:+':'$PATH':'}
    PATH=${PATH/':''/nix/store/1lbc6v5p1a3rn4rjaqnz0694xfbq8dxq-systemd-256.4/bin'':'/':'}
    PATH='/nix/store/1lbc6v5p1a3rn4rjaqnz0694xfbq8dxq-systemd-256.4/bin'$PATH
    PATH=${PATH#':'}
    PATH=${PATH%':'}
    export PATH
    PATH=${PATH:+':'$PATH':'}
    PATH=${PATH/':''/nix/store/8ywpryh3ngx85xwncilv1hgvk60ickl1-getent-glibc-2.39-52/bin'':'/':'}
    PATH='/nix/store/8ywpryh3ngx85xwncilv1hgvk60ickl1-getent-glibc-2.39-52/bin'$PATH
    PATH=${PATH#':'}
    PATH=${PATH%':'}
    export PATH
    PATH=${PATH:+':'$PATH':'}
    PATH=${PATH/':''/nix/store/izpf49b74i15pcr9708s3xdwyqs4jxwl-bash-5.2p32/bin'':'/':'}
    PATH='/nix/store/izpf49b74i15pcr9708s3xdwyqs4jxwl-bash-5.2p32/bin'$PATH
    PATH=${PATH#':'}
    PATH=${PATH%':'}
    export PATH
    exec -a "$0" "/nix/store/cvkr6mldlfg5l03ziks21szhjarm3sh1-goss-0.4.8/bin/.goss-wrapped"  "$@"
    ```
    
    With this patch:
    
    ```
    ❯ dgoss edit e321bdcd5137
    INFO: Starting podman container
    INFO: Container ID: 52ce9790
    INFO: Run goss add/autoadd to add resources
    
    sh-5.2# goss
    NAME:
       goss - Quick and Easy server validation
    
    USAGE:
       goss [global options] command [command options] [arguments...]
    
    VERSION:
       v0.4.8
    
    COMMANDS:
       validate, v  Validate system
       serve, s     Serve a health endpoint
       render, r    render gossfile after imports
       autoadd, aa  automatically add all matching resource to the test suite
       add, a       add a resource to the test suite
       help, h      Shows a list of commands or help for one command
    
    GLOBAL OPTIONS:
       --log-level value, --loglevel value, -L value, -l value  Goss log verbosity level (default: "INFO") [$GOSS_LOGLEVEL]
       --gossfile value, -g value                               Goss file to read from / write to (default: "./goss.yaml") [$GOSS_FILE]
       --vars value                                             json/yaml file containing variables for template [$GOSS_VARS]
       --vars-inline value                                      json/yaml string containing variables for template (overwrites vars) [$GOSS_VARS_INLINE]
       --package value                                          Package type to use [apk, dpkg, pacman, rpm]
       --help, -h                                               show help
       --version, -v                                            print the version
    ```
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- pkgs/by-name/dg/dgoss/package.nix
+++ pkgs/by-name/dg/dgoss/package.nix
@@ -17,7 +17,7 @@ resholve.mkDerivation rec {
   dontBuild = true;
 
   installPhase = ''
-    sed -i '2i GOSS_PATH=${goss}/bin/goss' extras/dgoss/dgoss
+    sed -i '2i GOSS_PATH=${goss}/bin/.goss-wrapped' extras/dgoss/dgoss
     install -D extras/dgoss/dgoss $out/bin/dgoss
   '';
 
